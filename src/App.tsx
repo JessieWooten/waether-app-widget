9c91397016f8fbf8eec133ca18c0166e35d2477e
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-21 18:35:49 -0500

    wip: layout and applying theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { IForecast } from "./models/Forecast";
+import { fetchForecastByLocationName } from "./api/WeatherServiceApi";
+import Theme from "./themes/ThemeWrapper";
 
 function App() {
+  const [forecast, setForecast] = useState<IForecast | undefined>(undefined);
+  // const [errorMessage, setErrorMessage] = useState("");
+  useEffect(() => {
+    (async () => {
+      try {
+        const forecast = await fetchForecastByLocationName("nashville,tn");
+        setForecast(forecast);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Theme>
+      <WidgetContainer className="App">
+        {JSON.stringify(forecast)}
+      </WidgetContainer>
+    </Theme>
   );
 }
 
 export default App;
+
+const WidgetContainer = styled.div`
+  max-width: 500px;
+  height: 280px;
+  background: ${(props) => props.theme.backgroundColor};
+  padding: 24px;
+  border-radius: 20px;
+  overflow: hidden;
+`;
