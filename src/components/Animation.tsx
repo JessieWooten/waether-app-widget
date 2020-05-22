6a41a403b965405f991a34fcb7661b6b6afa0053
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-22 15:26:20 -0500

    enhance: add BlurInBottom

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -36,6 +36,25 @@ export const BlurInTop = styled.div<{ duration?: string; delay?: string }>`
     }
   }
 `;
+export const BlurInBottom = styled.div<{ duration?: string; delay?: string }>`
+  animation: blurIn;
+  animation-timing-function: ease-in-out;
+  animation-delay: ${(props) => props.delay || 0};
+  animation-duration: ${(props) => props.duration || "400ms"};
+  animation-fill-mode: both;
+  @keyframes blurIn {
+    from {
+      filter: blur(4px);
+      opacity: 0;
+      transform: translate3d(0, 40%, 0);
+    }
+    to {
+      transform: translate3d(0, 0, 0);
+      filter: blur(0);
+      opacity: 1;
+    }
+  }
+`;
 export const BlurInLeft = styled.div<{ duration?: string; delay?: string }>`
   animation: blurIn;
   animation-timing-function: ease-in-out;
