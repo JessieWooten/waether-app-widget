1b29eaff3edcb756825b4a208076d9084d697cf7
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-22 03:24:38 -0500

    enhance: add Vertical center, center right and space between

--- src/components/base/Position.tsx
+++ src/components/base/Position.tsx
@@ -1,7 +1,22 @@
 import styled from "styled-components";
 
-export const centerContent = styled.div`
+export const CenterContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+export const VerticalCenterContent = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const VerticalCenterRightContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+export const VerticalCenterSpaceBetween = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
