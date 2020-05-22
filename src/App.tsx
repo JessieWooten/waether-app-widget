1862ad1903daf3117481945b295dd43a2aa37d11
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-22 03:23:11 -0500

    feat: add settings page

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,54 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { lighten, rgba } from "polished";
 import { IForecast, IForecastImage } from "./models/Forecast";
-import { fetchForecastByLocationName } from "./api/WeatherServiceApi";
+import {
+  fetchForecastByLocationName,
+  fetchForecastByCoordinates,
+  getBrowserLocation,
+} from "./api/WeatherServiceApi";
 import Theme from "./themes/ThemeWrapper";
 import fetchThemeByName from "./themes/utils/fetchThemeByName";
 import ForecastText from "./components/ForecastText";
 import ForecastIconDisplay from "./components/ForecastIconDisplay";
-import { lighten, rgba } from "polished";
+import SettingsPage from "./components/SettingsPage";
+import SettingsDots from "./components/SettingsDots";
 
 function App() {
+  let location = window.localStorage.getItem("wwjw-location");
   const [forecast, setForecast] = useState<IForecast | undefined>(undefined);
   const [image, setImage] = useState<IForecastImage | undefined>(undefined);
-  // const [errorMessage, setErrorMessage] = useState("");
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+  const [isCelcius, setIsCelcius] = useState(
+    window.localStorage.getItem("wwjw-celcius") === "true"
+  );
+  const updateLocation = (newLocation: string) => {
+    window.localStorage.setItem("wwjw-location", newLocation);
+    location = newLocation;
+  };
+  const [errorMessage, setErrorMessage] = useState("");
   useEffect(() => {
     (async () => {
       try {
-        const forecastResponse = await fetchForecastByLocationName(
-          "nashville,tn"
-        );
+        setErrorMessage("");
+        let forecastResponse;
+        if (location)
+          forecastResponse = await fetchForecastByLocationName(location);
+        else {
+          const coords = await getBrowserLocation();
+          forecastResponse = await fetchForecastByCoordinates(
+            coords.latitude,
+            coords.longitude
+          );
+        }
         setForecast(forecastResponse.forecast);
         setImage(forecastResponse.image);
       } catch (e) {
+        setErrorMessage(e);
         console.error(e);
       }
     })();
-  }, []);
+  }, [location]);
   const themeStyles = forecast ? fetchThemeByName(forecast.icon) : undefined;
   return (
     <Theme themeStyles={themeStyles}>
@@ -33,8 +57,18 @@ function App() {
         backgroundImage={image?.imgUrl || "#"}
         icon={forecast?.icon || ""}
       >
-        {forecast && <ForecastText forecast={forecast} />}
+        {forecast && <ForecastText isCelcius={isCelcius} forecast={forecast} />}
         {forecast && <ForecastIconDisplay forecast={forecast} />}
+        {isSettingsOpen ? (
+          <SettingsPage
+            isCelcius={isCelcius}
+            toggleIsCelcius={() => setIsCelcius(!isCelcius)}
+            closePage={() => setIsSettingsOpen(false)}
+            updateLocation={updateLocation}
+          />
+        ) : (
+          <SettingsDots onClick={() => setIsSettingsOpen(true)} />
+        )}
       </WidgetContainer>
     </Theme>
   );
@@ -60,6 +94,7 @@ const WidgetContainer = styled.div<{ backgroundImage: string; icon: string }>`
   justify-content: space-between;
   padding: 0 2rem;
   &::after {
+    pointer-events: none;
     content: "";
     width: 100%;
     height: 100%;
@@ -68,7 +103,7 @@ const WidgetContainer = styled.div<{ backgroundImage: string; icon: string }>`
         ? rgba(0, 0, 0, 0.5)
         : props.icon.includes("snow")
         ? rgba(255, 255, 255, 0.2)
-        : "transparent"};
+        : rgba(props.theme.background, 0.3)};
     position: absolute;
     top: 0;
     left: 0;
