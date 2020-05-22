9ce9ed4da248f662887121d5329281fec2a50e1e
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-21 22:41:49 -0500

    feat: build out text components

--- src/components/base/Typography.tsx
+++ src/components/base/Typography.tsx
@@ -1,7 +1,29 @@
 import styled from "styled-components";
 
-export const Temperature = styled.h2``;
+export const Temperature = styled.h2`
+  font-size: 10rem;
+  line-height: 10rem;
+  font-weight: 300;
+`;
 
-export const Heading = styled.h3``;
+export const Heading = styled.h3`
+  font-size: 1.5rem;
+  font-weight: 400;
+  padding: 0.5rem 0;
+`;
 
-export const Text = styled.p``;
+export const Text = styled.p`
+  font-size: 1rem;
+  font-weight: 400;
+  padding: 0 0 0.5rem 0;
+`;
+
+export const Divider = styled.hr`
+  display: block;
+  height: 1px;
+  border: none;
+  width: 100%;
+  background: ${(props) => props.theme.textColor};
+  opacity: 0.3;
+  margin: 0 0 0.5rem 0;
+`;
