a851ed654406d51aa2ee8921196b8146df643998
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-21 22:39:48 -0500

    feat: Layout Main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { IForecast } from "./models/Forecast";
+import { IForecast, IForecastImage } from "./models/Forecast";
 import { fetchForecastByLocationName } from "./api/WeatherServiceApi";
 import Theme from "./themes/ThemeWrapper";
+import fetchThemeByName from "./themes/utils/fetchThemeByName";
+import ForecastText from "./components/ForecastText";
+import ForecastIconDisplay from "./components/ForecastIconDisplay";
+import { rgba, darken } from "polished";
 
 function App() {
   const [forecast, setForecast] = useState<IForecast | undefined>(undefined);
+  const [image, setImage] = useState<IForecastImage | undefined>(undefined);
   // const [errorMessage, setErrorMessage] = useState("");
   useEffect(() => {
     (async () => {
       try {
-        const forecast = await fetchForecastByLocationName("nashville,tn");
-        setForecast(forecast);
+        const forecastResponse = await fetchForecastByLocationName(
+          "nashville,tn"
+        );
+        setForecast(forecastResponse.forecast);
+        setImage(forecastResponse.image);
       } catch (e) {
         console.error(e);
       }
     })();
   }, []);
+  const themeStyles = forecast ? fetchThemeByName(forecast.icon) : undefined;
   return (
-    <Theme>
-      <WidgetContainer className="App">
-        {JSON.stringify(forecast)}
+    <Theme themeStyles={themeStyles}>
+      <WidgetContainer className="App" backgroundImage={image?.imgUrl || "#"}>
+        {forecast && <ForecastText forecast={forecast} />}
+        {forecast && <ForecastIconDisplay forecast={forecast} />}
       </WidgetContainer>
     </Theme>
   );
@@ -28,11 +38,18 @@ function App() {
 
 export default App;
 
-const WidgetContainer = styled.div`
-  max-width: 500px;
-  height: 280px;
-  background: ${(props) => props.theme.backgroundColor};
-  padding: 24px;
+const WidgetContainer = styled.div<{ backgroundImage: string }>`
+  position: relative;
+  width: 480px;
+  height: 260px;
+  background: ${(props) => props.theme.background};
+  background-image: ${(props) => `url(${props.backgroundImage})`};
+  background-position: center;
+  background-size: cover;
   border-radius: 20px;
   overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 2rem;
 `;
