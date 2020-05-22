b2198460e0c015f9513453107249a4b5a9e940c9
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-05-21 22:39:14 -0500

    refactor: update mock api call. Add IForecastResponse interface

--- src/api/WeatherServiceApi.ts
+++ src/api/WeatherServiceApi.ts
@@ -1,26 +1,40 @@
-import { IForecast, ForecastIcon } from "../models/Forecast";
+import { IForecast, ForecastIcon, IForecastImage } from "../models/Forecast";
 
-const API_URL = "";
+const API_URL = "https://woot-weather-application.herokuapp.com/weather";
 
-const fetchForecast = async (url: string): Promise<IForecast> => {
-  const forecast: IForecast = {
-    temperature: 0,
-    summary: "string",
-    icon: ForecastIcon.CLEAR_DAY,
-    location: "string",
-    precipProbability: 0,
+export interface IForecastResponse {
+  location?: string;
+  forecast: IForecast;
+  image: IForecastImage;
+}
+
+const fetchForecast = async (url: string): Promise<IForecastResponse> => {
+  const forecast: IForecastResponse = {
+    location: "Nashville, Tennessee, United States",
+    forecast: {
+      temperature: 64.87,
+      summary: "Rain today through Wednesday.",
+      icon: ForecastIcon.RAIN,
+      precipProbability: 0,
+    },
+    image: {
+      id: "YUGSnfrliPk",
+      imgUrl:
+        "https://images.unsplash.com/photo-1521147923319-b7fbf0b31c6f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEzNjU0MH0",
+      photographer: "Joel Tasche",
+    },
   };
-  return new Promise((resolve) => setTimeout(() => resolve(forecast), 2000));
+  return new Promise((resolve) => setTimeout(() => resolve(forecast), 0));
 };
 
 export const fetchForecastByCoordinates = async (
   lat: string,
   long: string
-): Promise<IForecast> => {
+): Promise<IForecastResponse> => {
   return fetchForecast("");
 };
 export const fetchForecastByLocationName = async (
   name: string
-): Promise<IForecast> => {
+): Promise<IForecastResponse> => {
   return fetchForecast("");
 };
